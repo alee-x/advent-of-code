8e603e2dc664f72bf9fad6b472c7e5d904e75816
Alex <a.s.lee@swansea.ac.uk>
2021-12-01 12:00:52 +0000

    Refactored Day 1 and also added timer function to verify code runs with the time limit.

--- day1/challenge1-increasing-depth.py
+++ day1/challenge1-increasing-depth.py
@@ -1,4 +1,7 @@
 import numpy as np
+from timer import timer, get_timer
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 def readfile():
@@ -8,21 +11,22 @@ def readfile():
     return measures
 
 
+@timer('function:partone', unit='s')
 def partone():
     depth_measures = readfile()
-    is_increasing = [depth_measures[x] - depth_measures[x - 1] for x in range(1, len(depth_measures))]
-    return len(list(filter(lambda x: (x > 0), is_increasing)))
+    num_increasing = len([x for x in range(1, len(depth_measures)) if depth_measures[x] - depth_measures[x - 1] > 0])
+    return num_increasing
 
 
+@timer('function:parttwo', unit='s')
 def parttwo():
     depth_measures = readfile()
     sliding_window = [np.sum([depth_measures[x], depth_measures[x + 1], depth_measures[x + 2]])
                       for x in range(0, len(depth_measures) - 2)]
-    is_increasing = [sliding_window[x] - sliding_window[x - 1] for x in range(1, len(sliding_window))]
-    return len(list(filter(lambda x: (x > 0), is_increasing)))
+    num_increasing = len([x for x in range(1, len(sliding_window)) if sliding_window[x] - sliding_window[x - 1] > 0])
+    return num_increasing
 
 
 if __name__ == "__main__":
     print(partone())
     print(parttwo())
-    
